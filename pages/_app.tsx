1d98ac2bd8041e9214a901da3b4b85ea1c971200
Wahyu NT <wahyuntx28@gmail.com>
2024-04-17 07:51:23 +0700

    Update _app.tsx to include custom CSS, Bootstrap, and font-awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import "@/styles/globals.css";
+
+import "../styles/custom.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Arc Play</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+          crossOrigin="anonymous"
+        />
+        <link rel="icon" type="image/x-icon" href="/img/IconArc-play.png"></link>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossOrigin="anonymous" />
+      </Head>
+      {/* BootstrapJS  */}
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossOrigin="anonymous" async />
+      <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
