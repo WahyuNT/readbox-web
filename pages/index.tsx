0e65b7fbc78fb13f0df6e1f1670d94e3db33672d
Wahyu NT <wahyuntx28@gmail.com>
2024-04-18 08:38:10 +0700

    Update index.tsx and custom.css to fix styling issues and add new section

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,37 +49,57 @@ export default function Home() {
       <section className="section2 mt-4">
         <div className="container">
 
-          <h2 className="text-center fw-bold mb-5">OVERVIEW</h2>
-          <div className="d-flex justify-content-center">
-            <div className="col-4 px-3">
-              <div className="card text-center card-overview">
-                <div className="card-body">
+          <h2 className="text-center fw-bold mb-5 text-black">OVERVIEW</h2>
+          <div className="d-flex justify-content-between">
+            <div className="col-4 px-5">
+              <div className="card text-center card-overview  px-2 h-100">
+                <div className="card-body ">
                   <img src="/img/icon1.png" className="my-2" alt="" />
-                  <h5 className="fw-bold">Apa Itu ReadBox?</h5>
+                  <h5 className="fw-bold text-black">Apa Itu ReadBox?</h5>
                   <p>ReadBox adalah platform untuk membaca komik dan novel berbasis website dan aplikasi. ReadBox dapat membantu anda berkembang menjadi kreator hebat.</p>
                 </div>
               </div>
             </div>
-            <div className="col-4 px-3">
-              <div className="card text-center card-overview">
+            <div className="col-4 px-5">
+              <div className="card text-center card-overview px-2 h-100">
                 <div className="card-body">
                   <img src="/img/icon2.png" className="my-2" alt="" />
-                  <h5 className="fw-bold">Cara ReadBox Bekerja?</h5>
+                  <h5 className="fw-bold text-black">Cara ReadBox Bekerja?</h5>
                   <p>Kreator menciptakan karya komik atau novel lalu mengunggaanya di Readbox,lalu pembaca dapat mengakses karya tersebut melalui aplikasi maupun website ReadBox.</p>
                 </div>
               </div>
             </div>
-            <div className="col-4 px-3">
-              <div className="card text-center card-overview">
+            <div className="col-4 px-5">
+              <div className="card text-center card-overview px-2 h-100">
                 <div className="card-body">
                   <img src="/img/icon3.png" className="my-2" alt="" />
-                  <h5 className="fw-bold">Cara Menciptakan Penghasilan?</h5>
+                  <h5 className="fw-bold text-black">Cara Menciptakan Penghasilan?</h5>
                   <p>Keator dapat memiliki penghasilan melalui iklan yang akan ditampilkan oleh ReadBox dari para pengiklan yang ingin product nya ditampiin pada karyaa kreator.</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </section>
+      <section className="section3">
+        <div className="container">
+
+          <div className="d-flex justify-content-between align-items-center">
+         
+
+              <div className="col-">
+                <img src="/img/app2.png" alt="" />
+              </div>
+              <div className="col ms-5 pe-5">
+                <h1 className="fw-bold text-black">Segera Hadir di Mobile</h1>
+                <p>ReadBox juga akan hadir dalam aplikasi yang dapat di unduh di perangkat iOS dan Android agar mempermudah pengguna untuk mengakses ReadBox</p>
+              </div>
+         
+          </div>
+        </div>
+      </section>
+      <section>
+        sadasd
       </section>
     </>
   );
